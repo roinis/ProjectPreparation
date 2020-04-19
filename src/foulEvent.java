f76b1800f95b492a5291010ef69eec5c33376fd8
roinis <roinisan@gmail.com>
2020-04-19 16:07:41 +0300

    added foul logic

--- src/foulEvent.java
+++ src/foulEvent.java
@@ -1,2 +1,22 @@
-public class foulEvent extends gameEvent {
+import java.sql.Time;
+
+public class foulEvent extends gameEvent{
+    player fouledPlayer;
+
+    public foulEvent(Time eventGameTime, team team, player player, player fouledPlayer) {
+        super(eventGameTime, team, player);
+        this.fouledPlayer = fouledPlayer;
+    }
+
+    @Override
+    public String toString() {
+        return "The Player "
+                + eventPlayer.getName()
+                + "of team " + eventTeam.getName()
+                + " Committed a foul on "
+                + fouledPlayer.getName()
+                + " at "
+                + eventGameTime + ".";
+    }
 }
+
