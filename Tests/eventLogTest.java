18350b66df520ed47229a7d4e6e06fad2cf937fd
roinis <roinisan@gmail.com>
2020-04-19 18:05:38 +0300

    tests

---
+++ Tests/eventLogTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+public class eventLogTest {
+
+    @org.junit.Test
+    public void register() {
+    }
+
+    @org.junit.Test
+    public void unregister() {
+    }
+
+    @org.junit.Test
+    public void notifyObserver() {
+    }
+
+    @org.junit.Test
+    public void addEvent() {
+    }
+
+    @org.junit.Test
+    public void getNewEvent() {
+    }
+
+    @org.junit.Test
+    public void setNewEvent() {
+    }
+}
