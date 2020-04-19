61f8a180972198ca17e404aaa2fdeef0e1c7da6f
galatedgi <gal.atedgi@gmail.com>
2020-04-19 18:52:59 +0300

    gal test

--- src/team.java
+++ src/team.java
@@ -1,9 +1,82 @@
-public class team {
-    public stadium getStadium() {
-        return null;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    enum Status{open,close}
+    private String teamName;
+    private List<TeamOwner> owners;
+    private  List<Player> players;
+    private List<Coach> coaches;
+    private List<TeamManager> managers;
+    private List<Member> members;
+    //jsdkls
+    private Status status;
+
+    public Team(String teamName,TeamOwner owner) {
+        this.teamName=teamName;
+        owners=new ArrayList<>();
+        players=new ArrayList<>();
+        coaches=new ArrayList<>();
+        managers=new ArrayList<>();
+        owners.add(owner);
+        this.status=Status.open;
     }
 
-    public String getName() {
-        return null;
+    public String getTeamName() {
+        return teamName;
     }
+
+    public void setTeamName(String teamName) {
+        if(status==Status.close){
+            System.out.println("the team is close");
+            return;
+        }
+        this.teamName = teamName;
+    }
+
+    public List<TeamOwner> getOwners() {
+        return owners;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public List<Coach> getCoaches() {
+        return coaches;
+    }
+
+    public List<TeamManager> getManagers() {
+        return managers;
+    }
+
+    public void addOwner(TeamOwner teamOwner){
+        if(status==Status.close){
+            System.out.println("the team is close");
+            return;
+        }
+        owners.add(teamOwner);
+    }
+
+    public void removeOwner(TeamOwner teamOwner) {
+        if(status==Status.close){
+            System.out.println("the team is close");
+            return;
+        }
+        if (owners.size() == 1) {
+            System.out.println("you cant' remove the owner");
+            return;
+        }
+        owners.remove(teamOwner);
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+
 }
