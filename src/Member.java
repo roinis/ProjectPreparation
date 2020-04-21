7df800b041b46b370af7b9cb119b2436d92072d2
roeie <roeie@post.bgu.ac.il>
2020-04-21 21:58:22 +0300

    "roei cohen 21.4 22:00"

--- src/Member.java
+++ src/Member.java
@@ -10,6 +10,7 @@ public class Member extends User implements Observer{
     private String full_name;
     private HashMap<String,Job> jobs;
     private List<Event> eventList;
+    private boolean online;
 
     public Member(String user_name,String user_password,String user_id,String full_name){
         this.user_name=user_name;
@@ -18,10 +19,52 @@ public class Member extends User implements Observer{
         this.full_name=full_name;
         jobs = new HashMap<>();
         eventList = new ArrayList<>();
+        online=false;
+    }
+
+
+    public void editPersonalInformation(){//1-username|2-password|3-id number|4-fullname
+        System.out.println("Edit Personal Information:");
+        System.out.println("What would you like to edit?(Please enter the number of the field)");
+        System.out.println("1.User Name");
+        System.out.println("2.Password");
+        System.out.println("3.ID Number");
+        System.out.println("4.Full Name");
+        Scanner sc = new Scanner(System.in);
+        int choise = Integer.parseInt(sc.nextLine());
+        String input ="";
+        boolean result = false;
+        UsersInformation usersInformation = new UsersInformation();
+        switch (choise){
+            case 1:
+                System.out.println("Please enter the new user name:");
+                input = sc.nextLine();
+                result = usersInformation.editInformation(1,user_name,input);
+            case 2:
+                System.out.println("Please enter the new password:");
+                input = sc.nextLine();
+                result  = usersInformation.editInformation(2,user_name,input);
+            case 3:
+                System.out.println("Please enter the new id number:");
+                input = sc.nextLine();
+                result = usersInformation.editInformation(3,user_name,input);
+            case 4:
+                System.out.println("Please enter the new full name:");
+                input = sc.nextLine();
+                result = usersInformation.editInformation(3,user_name,input);
+        }
+        if(result==false)
+            System.out.println("There was a problem changing the information of this Member");
+        else
+            System.out.println("Information have been changed successfully!");
     }
 
     public String getFull_name(){
         return full_name;
+    }
+
+    public void setOnline(boolean online){
+        this.online=online;
     }
 
     public HashMap<String,Job> getJobsList(){
@@ -39,6 +82,10 @@ public class Member extends User implements Observer{
             jobs.put(job.getJobName(), job);
             addJobToFile(job);
         }
+    }
+
+    public void logout(){
+        online=false;
     }
 
     public boolean removeJob(String job_name){
@@ -114,6 +161,30 @@ public class Member extends User implements Observer{
 
         }catch (IOException e){
             e.fillInStackTrace();
+        }
+    }
+
+    public void followPersonalPage(){//need to finish
+        System.out.println("Please chose the number of one of the following you would like to follow:");
+        System.out.println("1.Teams");
+        System.out.println("2.Players");
+        System.out.println("3.Coaches");
+        Scanner sc = new Scanner(System.in);
+        int choise = Integer.parseInt(sc.nextLine());
+        switch (choise){
+            case 1:
+                System.out.println("Please chose one the number of one the following Teams you would like to follow after:");
+                List<Team> teamsList = (List<Team>)AlphaSystem.getSystem().GetAllFromDB(4);
+                for(int i = 0; i<teamsList.size();i++){
+                    System.out.println((i+1)+". "+teamsList.get(i).getTeamName());
+                }
+                choise = Integer.parseInt(sc.nextLine());
+                if(choise<1||choise>teamsList.size())
+                    return;
+                Team chosenTeam = teamsList.get(choise-1);
+                chosenTeam.register(this);
+            case 2:
+
         }
     }
 }
