e8d54ab2b88b501a5c567983f46122585d0c6837
Amit Wolf <amitthewolf@hotmail.com>
2020-04-22 19:18:02 +0300

    SystemAdmin is now Member

--- src/SystemAdmin.java
+++ src/SystemAdmin.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
-public class SystemAdmin {
+public class SystemAdmin extends Member{
+
+    public SystemAdmin(String user_name, String user_password, String user_id, String full_name){
+        super(user_name, user_password, user_id, full_name);
+    }
 
     public void CloseTeam(Team TeamToClose){
         TeamToClose.setStatus(Team.Status.close);
